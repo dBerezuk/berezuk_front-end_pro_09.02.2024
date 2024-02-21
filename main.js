a8f093a16b694ad7983bb155b8b038120557d07d
Denis <d_berezuk@icloud.com>
2024-02-21 18:56:56 +0200

    Saving dz

---
+++ main.js
@@ -0,0 +1,5 @@
+const numberOne = +prompt('Введите первое число');
+const numberTwo = +prompt('Введите второе число');
+const numberThree = +prompt('Введите третие число');
+
+alert( (numberOne + numberTwo + numberThree) / 3);
