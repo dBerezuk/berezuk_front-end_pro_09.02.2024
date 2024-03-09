e68b6203ea956d0399e338c961c39c2dc96c4065
Denis <d_berezuk@icloud.com>
2024-03-09 23:03:06 +0200

    edit add decrement

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const pow = (num, degree) => {
     if(degree === 1) {
         return num;
     } else {
-        return num * pow(num, degree - 1);
+        return num * pow(num, --degree);
     }
 }
 
