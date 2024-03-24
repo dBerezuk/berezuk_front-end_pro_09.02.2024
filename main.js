f983be25ac926ad74e1128e677718d892819491e
Denis <d_berezuk@icloud.com>
2024-03-24 15:14:21 +0200

    Saving dz

---
+++ main.js
@@ -0,0 +1,12 @@
+'use strict';
+
+(function (users) {
+    if (!(Array.isArray(users) && users.length !== 0)) return;
+
+    const { phones, sum, totalSum } = usersAction(users);
+
+    console.table(phones);
+    console.log(`Общая сумма балансов пользователей у которых баланс больше $2000: ${sum}
+-----    
+Общая сумма балансов пользователей: ${totalSum}`);
+})(users);
