62f5eccecf7b85cff349a90e7118847be2f61e05
Denis <d_berezuk@icloud.com>
2024-03-24 15:53:06 +0200

    edit main.js, users.js, create file constants.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
 (function (users) {
     if (!(Array.isArray(users) && users.length !== 0)) return;
 
-    const { phones, sum, totalSum } = usersAction(users);
+    const { phones, sum, totalSum } = usersAction();
 
     console.table(phones);
     console.log(`Общая сумма балансов пользователей у которых баланс больше $2000: ${sum}
